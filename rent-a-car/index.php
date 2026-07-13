825e95cb2278753f58e515984b6d63ca7cbd42d3
Pedro Olivier <pedro0olivier@gmail.com>
2026-07-13 07:53:21 -0300

    Update index.php

--- rent-a-car/index.php
+++ rent-a-car/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/includes/config.php';
 
-// Se já estiver logado, vai direto pra Home
 if (usuarioEstaLogado()) {
     header('Location: ' . BASE_URL . 'home.php');
     exit;
