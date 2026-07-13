218165da64f79f07cf10427d7047424920167445
Pedro Olivier <pedro0olivier@gmail.com>
2026-07-13 07:54:38 -0300

    Update carro_detalhe.php

--- rent-a-car/carro_detalhe.php
+++ rent-a-car/carro_detalhe.php
@@ -19,7 +19,6 @@ $protecoes = $pdo->query('SELECT * FROM protecoes ORDER BY id')->fetchAll();
 $adicionais = $pdo->query('SELECT * FROM adicionais ORDER BY id')->fetchAll();
 $usuario = usuarioAtual();
 
-// Datas padrão: retirada hoje, devolução em 7 dias (para já ilustrar a promoção Black Nov)
 $hoje = date('Y-m-d\TH:i', strtotime('+1 day 10:00'));
 $emSeteDias = date('Y-m-d\TH:i', strtotime('+8 days 10:00'));
 
